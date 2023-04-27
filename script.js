ef795000a7d708dbfb366fe9b8d09e3404e93674
Kayhan Turker <47871034+kayhan-turker@users.noreply.github.com>
2023-04-27 12:42:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,33 @@ function updateTimer() {
     var targetDate = new Date("May 12, 2023 12:00:00");
 
     // Calculate the time difference in milliseconds
-    var timeDifference = targetDate - now - 12 * 60 * 60 * 1000;
+    var timeDifference = targetDate - now - (12 * 60 * 60 + 60 * 60 * 24 * 0) * 1000;
+    timeDifference = (timeDifference / (1000 * 60 * 60 * 24));
 
     if (timeDifference < 0) {
         timeDifference = 0;
     }
 
     // Calculate the time update value (100% divided by time difference in seconds)
-    var timeLeftValue = ((timeDifference) / (1000 * 60 * 60 * 24)).toFixed(2);
     
-    var invertedPercentValue = (100 * (1000 * 60 * 60 * 24) / (timeDifference)).toFixed(4);
+    var invertedPercentValue = (100 / timeDifference).toFixed(4);
     var invertedPercentLastTwo = invertedPercentValue.slice(-2);
-    invertedPercentValue = Math.floor(invertedPercentValue * 100) / 100;
+    invertedPercentValue = invertedPercentValue.slice(0, -2);
 
     // Display the time update value on the webpage
-    document.getElementById("timeLeft").innerHTML = timeLeftValue + " days left";
+
+    var timeRemainingString;
+
+    if (timeDifference > 3) {
+        var timeLeftValue = timeDifference.toFixed(2);
+        timeRemainingString = "Countdown: " + timeLeftValue + " days left";
+    }
+    else {
+        var timeLeftValue = (timeDifference * 24).toFixed(2);
+        timeRemainingString = timeLeftValue + " Hours Remain";
+    }
+
+    document.getElementById("timeLeft").innerHTML = timeRemainingString;
     document.getElementById("invertedPercent").innerHTML = invertedPercentValue;
     document.getElementById("invertedPercentLastTwo").innerHTML = invertedPercentLastTwo + "%";
 }
