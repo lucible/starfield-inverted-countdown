2f640fa4b084ec6a9bf0372d47d11df29d49003e
Kayhan Turker <47871034+kayhan-turker@users.noreply.github.com>
2023-05-08 00:06:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 
+var skipTime = 3.996;
+var moon_opacity = 0.12;
+
+
 // Function to update the timer value_days
 
 function updateTimer() {
@@ -9,16 +13,19 @@ function updateTimer() {
 
     var now = new Date();
     var targetDate = new Date("May 12, 2023 12:00:00");
-    var timeDifference = targetDate - now - (12 * 60 * 60 + 60 * 60 * 24 * 0) * 1000;
+    var timeDifference = targetDate - now - (12 * 60 * 60 + 60 * 60 * 24 * skipTime) * 1000;
     timeDifference = (timeDifference / (1000 * 60 * 60 * 24));
+
+    // Update description
+
+    var descString = getDescription(timeDifference);
+
+    // Calculate the inverted value_days (100% divided by time difference in seconds)
 
     if (timeDifference < 0) {
         timeDifference = 0;
     }
 
-
-    // Calculate the inverted value_days (100% divided by time difference in seconds)
-    
     var invertedPercentvalue_days = (100 / timeDifference).toFixed(4);
     var invertedPercentLastTwo = "";
 
@@ -28,8 +35,8 @@ function updateTimer() {
     }
     else
     {
-        invertedPercentLastTwo = "%";
-        invertedPercentRemaining = "Infinity";
+        invertedPercentLastTwo = "";
+        invertedPercentRemaining = "Infinity%";
     }
 
 
@@ -37,7 +44,6 @@ function updateTimer() {
 
     var timeRemainingString;
     var invertedTimeString = "Inverted Time: " + invertedPercentRemaining;
-    var descString = "Out of the days left right now, <br>what percent of that will be covered <br>with one more day of waiting.";
 
     if (timeDifference > 3) {
         var days = timeDifference.toFixed(2);
@@ -55,27 +61,34 @@ function updateTimer() {
         timeRemainingString = "Time Remaining: " + hours.toString().padStart(2, '0') + " : ";
         timeRemainingString += minutes.toString().padStart(2, '0') + " : ";
         timeRemainingString += seconds.toString().padStart(2, '0');
-
-        descString = "Save and return to the Dawn of<br>the First Day?<br>&nbsp;&nbsp;&nbsp;Yes<br>&nbsp;&nbsp;&nbsp;No";
     }
-
-    if (timeDifference == 0)
-        descString = ""
 
     document.getElementById("timeLeft").innerHTML = timeRemainingString;
     document.getElementById("invertedPercent").innerHTML = invertedTimeString;
     document.getElementById("invertedPercentLastTwo").innerHTML = invertedPercentLastTwo;
     document.getElementById("description").innerHTML = descString;
 
+}
+
+
+
+function updateTimerSlow() {
+
+
+    // Calculate the time difference
+
+    var now = new Date();
+    var targetDate = new Date("May 12, 2023 12:00:00");
+    var timeDifference = targetDate - now - (12 * 60 * 60 + 60 * 60 * 24 * skipTime) * 1000;
+    timeDifference = (timeDifference / (1000 * 60 * 60 * 24));
+
+    if (timeDifference < 0) {
+        timeDifference = 0;
+    }
 
     // calculate moon size
 
-    var thirdDaySize = 0.11;
-    var endSize = 4.35;
-    var factor = - 3 * thirdDaySize / (endSize - thirdDaySize);
-
-    var size = 3 * thirdDaySize / timeDifference;
-    //var size = factor * endSize / (-timeDifference + factor);    
+    var size = 3 * 0.11 / timeDifference;  
     var imgs = document.getElementsByClassName("image-moon");
 
     for (var i = 0; i < imgs.length; i++) {
@@ -104,15 +117,13 @@ function updateTimer() {
 
     // set moon color
 
-    var totalOpacity = 0.12;
-
-    document.getElementById("moon").style.opacity = totalOpacity * (1 - value_days);
-    document.getElementById("moon-red").style.opacity = totalOpacity * value_days;
+    document.getElementById("moon").style.opacity = moon_opacity * (1 - value_days);
+    document.getElementById("moon-red").style.opacity = moon_opacity * value_days;
 
     if (value_seconds > 0) {
         var value_days = value_hours;
         document.getElementById("moon").style.opacity = 0;
-        document.getElementById("moon-red").style.opacity = totalOpacity * (1 - value_seconds);
+        document.getElementById("moon-red").style.opacity = moon_opacity * (1 - value_seconds);
         document.getElementById("image-done").style.opacity = value_seconds;
     }
 }
@@ -139,6 +150,75 @@ function interpolateHexColor(color1, color2, value_days) {
 }
 
 
-updateTimer();
+function getDescription(timeDifference) {
+    var descString = "";
 
-setInterval(updateTimer, 30);
+    if (timeDifference > 3)
+        descString = "Out of the days left right now, <br>what percent of that will be covered <br>with one more day of waiting.";
+    else if ((3 - 6 / 24 ) < timeDifference && timeDifference <  (3 - 0 / 24 ))
+        descString = "In the land of Hyrule, there<br>echoes a legend. A legend held<br>dearly by the Royal Family that<br>tells of a boy...";
+
+    else if ((3 - 12 / 24 ) < timeDifference && timeDifference <  (3 - 6 / 24))
+        descString = "You've met with a terrible fate,<br>haven't you?";
+
+
+    else if ((3 - 14 / 24 ) < timeDifference && timeDifference <  (3 - 12 / 24))
+        descString = "You cowards! Do you actually<br>believe the moon will fall?";
+
+    else if ((3 - 16 / 24 ) < timeDifference && timeDifference <  (3 - 14 / 24))
+        descString = "You, too...<br>You're also looking for Kafei.";
+
+    else if ((3 - 18 / 24 ) < timeDifference && timeDifference <  (3 - 16 / 24))
+        descString = "How about...Grasshopper?<br>That's the name Romani gives you.";
+
+    else if ((2 - 5 / 24 ) < timeDifference && timeDifference <  (2 - 0 / 24))
+        descString = "I am sorry to trouble you late<br>at night. It's about him...Kafei.";
+
+    else if ((2 - 6 / 24 ) < timeDifference && timeDifference <  (2 - 5 / 24))
+        descString = "It's almost time for my sister to get<br>up, so I have to get back to bed...";
+
+
+    else if ((2 - 18 / 24 ) < timeDifference && timeDifference <  (2 - 16 / 24))
+        descString = "Green hat...<br>Green clothes...<br>Anju wrote about you in her letter.";
+
+    else if ((2 - 20 / 24 ) < timeDifference && timeDifference <  (2 - 18 / 24))
+        descString = "We have to take this detour through<br>ugly country...";
+
+    else if ((2 - 22 / 24 ) < timeDifference && timeDifference <  (2 - 20 / 24))
+        descString = "OK, Anju, we're leaving in the<br>evening for the ranch.";
+
+    else if ((1 - 6 / 24 ) < timeDifference && timeDifference <  (2 - 22 / 24))
+        descString = "That melody... It brings back<br>so many memories!";
+
+    else if ((1 - 10 / 24 ) < timeDifference && timeDifference <  (1 - 6 / 24))
+        descString = "Actually...<br>I know...<br>We're not safe here, either...";
+
+    else if ((1 - 16 / 24 ) < timeDifference && timeDifference <  (1 - 10 / 24))
+        descString = "I have decided to wait for him.<br>I've made my promise...";
+
+    else if ((1 - 19 / 24 ) < timeDifference && timeDifference <  (1 - 16 / 24))
+        descString = "I found him, green hat boy...<br>If he sees us, he'll run away for sure.<br>We'd better both hide here.";
+
+    else if ((1 - 21 / 24 ) < timeDifference && timeDifference <  (1 - 19 / 24))
+        descString = "He's trying to pull it out of our reach<br>and make his escape...<br>We've got to hurry!";
+
+    else if ((0 ) < timeDifference && timeDifference <  (6 / 24))
+        descString = "Save and return to the Dawn of<br>the First Day?<br>&nbsp;&nbsp;&nbsp;Yes<br>&nbsp;&nbsp;&nbsp;No";
+
+    else if ((- 6 / 24 ) < timeDifference && timeDifference <  (- 4 / 24 / 3600))
+        descString = "We shall greet the morning...<br>together.";
+
+
+    else
+        descString = "";
+    
+    return descString;
+
+}
+
+
+updateTimer();
+updateTimerSlow();
+
+setInterval(updateTimer, 10);
+setInterval(updateTimerSlow, 100);
